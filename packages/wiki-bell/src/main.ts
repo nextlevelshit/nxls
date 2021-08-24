d115bf2a22decc796ab4ef1afd22770841cb27d6
Michael Czechowski <mail@dailysh.it>
2021-08-24 20:49:19 +0200

    better heuristics (2h)

--- packages/wiki-bell/src/main.ts
+++ packages/wiki-bell/src/main.ts
@@ -16,9 +16,9 @@ app.get("/api", (req, res) => {
 		("0" + new Date().getHours()).slice(-2),
 		("0" + new Date().getMinutes()).slice(-2),
 	]
-	const year = parseInt(now.join("")).toString()
+	const time = parseInt(now.join("")).toString()
 
-	fetch(`https://en.wikipedia.org/api/rest_v1/page/html/${year}`, {
+	fetch(`https://en.wikipedia.org/api/rest_v1/page/html/${time}`, {
 		headers: {
 			"Api-User-Agent": "Michael W. Czechowski <mc@dailysh.it>",
 			accept: "application/json",
@@ -37,9 +37,32 @@ app.get("/api", (req, res) => {
 			const births = $('h2:contains("Births")').parent().find("ul > li")
 			const deaths = $('h2:contains("Deaths")').parent().find("ul > li")
 			const topics = [...events, ...births, ...deaths]
-			const list = events ?? topics ?? all
-			const coinFlip = Math.floor(Math.random() * (list.length + 1))
-			const random = cheerio(list[coinFlip]).html()
+			const list = topics.length ? topics : all
+			const coinFlip = Math.floor(Math.random() * list.length)
+			const randomEl = cheerio(list[coinFlip])
+			const random = {
+				html: randomEl.html(),
+				raw: randomEl.text(),
+			}
+
+			console.log(
+				cheerio(all)
+					.toArray()
+					.map((a) => {
+						return cheerio(a).text()
+					})
+			)
+
+			const closestEl = [...list].find((item) => {
+				if ((cheerio(item).text() as String).includes(time)) {
+					return true
+				}
+				return false
+			})
+			const closest = {
+				html: cheerio(closestEl).html(),
+				raw: cheerio(closestEl).text(),
+			}
 
 			// Oh man, sag schon, was soll das
 			// ein falsches wort nach dem and'ern
@@ -51,9 +74,10 @@ app.get("/api", (req, res) => {
 			// durchbricht diesen wundervollen ort
 
 			res.send({
-				year,
+				time,
 				now,
 				random,
+				closest,
 			})
 		})
 		.catch((err) => {
@@ -63,17 +87,9 @@ app.get("/api", (req, res) => {
 		})
 })
 
-const port = process.env.port || 3333
+const port = process.env.PORT || 3333
 
-const server = https
-	.createServer(
-		{
-			key: fs.readFileSync("./ssl/key.pem"),
-			cert: fs.readFileSync("./ssl/cert.pem"),
-		},
-		app
-	)
-	.listen(port, () => {
-		console.log(`Listening at https://wiki.bell:${port}/api`)
-	})
+const server = app.listen(port, () => {
+	console.log(`Listening at https://0.0.0.0:${port}/api`)
+})
 server.on("error", console.error)
